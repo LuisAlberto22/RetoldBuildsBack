7615cbceec0d3b7ae912bec6b8e5ba42aa172359
Luis Alberto Garcia Orozco <guichos-mx@outlook.com>
2024-09-17 13:31:07 -0600

    Instalacion de paquetes

--- Retold.Api/obj/Debug/net8.0/Retold.Api.AssemblyInfo.cs
+++ Retold.Api/obj/Debug/net8.0/Retold.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Retold.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d1a32e730ba3a87af7756471dfaab68cc0b3211")]
 [assembly: System.Reflection.AssemblyProductAttribute("Retold.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Retold.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
