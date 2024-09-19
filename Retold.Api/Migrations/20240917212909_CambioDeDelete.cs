4f713a21c9a65df67ceba093f94f701d6c48bf85
Luis Alberto Garcia Orozco <guichos-mx@outlook.com>
2024-09-18 23:08:23 -0600

    Migraciones de la base de datos

---
+++ Retold.Api/Migrations/20240917212909_CambioDeDelete.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Retold.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambioDeDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Users_UserId1",
+                table: "Likes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Users_UserId1",
+                table: "Likes",
+                column: "UserId1",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Users_UserId1",
+                table: "Likes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Users_UserId1",
+                table: "Likes",
+                column: "UserId1",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
---
+++ Retold.Api/Migrations/20240918041139_UserIdChange.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Retold.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserIdChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Builds_Users_UserId1",
+                table: "Builds");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Users_UserId1",
+                table: "Likes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Likes_UserId1",
+                table: "Likes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Builds_UserId1",
+                table: "Builds");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Likes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Builds");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Likes",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Builds",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_UserId",
+                table: "Likes",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Builds_UserId",
+                table: "Builds",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Builds_Users_UserId",
+                table: "Builds",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Users_UserId",
+                table: "Likes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Builds_Users_UserId",
+                table: "Builds");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Users_UserId",
+                table: "Likes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Likes_UserId",
+                table: "Likes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Builds_UserId",
+                table: "Builds");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Likes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Likes",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Builds",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Builds",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_UserId1",
+                table: "Likes",
+                column: "UserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Builds_UserId1",
+                table: "Builds",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Builds_Users_UserId1",
+                table: "Builds",
+                column: "UserId1",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Users_UserId1",
+                table: "Likes",
+                column: "UserId1",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
