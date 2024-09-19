be1dc4e9f0e686b407c18052ebe180deb1788e33
Luis Alberto Garcia Orozco <guichos-mx@outlook.com>
2024-09-18 23:44:48 -0600

    Registro de srvicios, nappers y repositorios

--- Retold.Api/Program.cs
+++ Retold.Api/Program.cs
@@ -1,11 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Retold.Api.Data;
+using Retold.Api.Mappers;
+using Retold.Api.Repositories;
+using Retold.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionStrings = builder.Configuration.GetConnectionString("DefaultConnection");
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddRepositories();
+builder.Services.AddServices();
+builder.Services.AddProfiles();
+
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+	options.UseSqlServer(connectionStrings);
+});
 
 var app = builder.Build();
 
